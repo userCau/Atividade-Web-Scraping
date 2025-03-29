98569b8f259c0cd2cb5a2ef3a1579d8e513590a2
Cauã Carvalho <110573973+userCau@users.noreply.github.com>
2025-03-29 19:12:02 -0300

    Add files via upload

---
+++ ScrapingFatecRl.js
@@ -0,0 +1,11 @@
+//Atividade: Obter informações ou mudar o estilo de uma página escolhida.
+
+//Apenas alterei a header da pagina e deixei a logo da Fatec Preta.
+ 
+function mudarheader(){
+    document.querySelector('.menu-link--vestibular').style.backgroundColor = "#2f2f22";
+    document.querySelector('.header').style.background = "#777676";
+    document.querySelector('.mainBar-menu').style.background = "#777676";
+    document.querySelector('.auxiliaryBar').style.background = "#636060";
+    document.querySelector(".header-logo").style.filter = "brightness(0)";
+}
